0e9c54a92756568f386a8960312665a5a47f909e
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-08-30 21:49:16 -0500

    Agregando funciones a la app con React

--- blog_platzi/src/components/App.js
+++ blog_platzi/src/components/App.js
@@ -1,6 +1,32 @@
 import React from 'react'
 
 const App = () => {
+
+  const ponerFilas = () => [
+    <tr>
+      <td>
+        Rodolfo
+      </td>
+      <td>
+        Rodolfo@saldivar.com
+      </td>
+      <td>
+        Rodolfo.com
+      </td>
+    </tr>,
+    <tr>
+      <td>
+        Ulises
+      </td>
+      <td>
+        15121185@tecmor.mx
+      </td>
+      <td>
+        platzi.com
+      </td>
+    </tr>
+  ]
+
   return (
     <div className='margen'>
       <table className='tabla'>
@@ -18,28 +44,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              Rodolfo
-          </td>
-            <td>
-              Rodolfo@saldivar.com
-          </td>
-            <td>
-              Rodolfo.com
-          </td>
-          </tr>
-          <tr>
-            <td>
-              Ulises
-          </td>
-            <td>
-              15121185@tecmor.mx
-          </td>
-            <td>
-              platzi.com
-          </td>
-          </tr>
+          { ponerFilas() }
         </tbody>
       </table>
     </div>
