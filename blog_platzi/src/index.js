d6c40dbde5b2385e1043830e0aca5471fdedba93
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-08-31 21:13:06 -0500

    Reducers

--- blog_platzi/src/index.js
+++ blog_platzi/src/index.js
@@ -6,12 +6,16 @@ import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import reducers from './reducers'
+
 const store = createStore(
-    {}, // Todos los reducers
+    reducers, // Todos los reducers
     {} // Estado inicial
 );
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={ store }>
+        <App />
+    </Provider>, 
     document.getElementById('root')
 );
