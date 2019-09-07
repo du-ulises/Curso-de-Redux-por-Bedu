0dfba30ef4c24f7eafec3b68b28d5c623c8153ff
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-09-07 12:45:07 -0500

    Inmutabilidad

--- blog_platzi/src/actions/publicacionesActions.js
+++ blog_platzi/src/actions/publicacionesActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { TRAER_POR_USUARIO, CARGANDO, ERROR } from '../types/publicacionesTypes';
+import * as usuariosTypes from '../types/usuariosTypes';
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
     const { usuarios } = getState().usuariosReducer;
@@ -7,11 +10,23 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
     const usuario_id = usuarios[key].id;
     
     const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`);
-    
+
     const publicaciones_actualizadas = [
         ...publicaciones,
         respuesta.data
     ];
+
+    const publicaciones_key = publicaciones_actualizadas.length - 1;
+    const usuarios_actualizados = [...usuarios];
+    usuarios_actualizados[key] = {
+        ...usuarios[key],
+        publicaciones_key
+    }
+
+    dispatch({
+        type: USUARIOS_TRAER_TODOS,
+        PAYLOAD: usuarios_actualizados
+    });
 
     dispatch({
         type: TRAER_POR_USUARIO,
