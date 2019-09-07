85fff71717202cc24d080d20f292c5e63a8528c4
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-09-07 14:31:21 -0500

    Estado con interacción

--- blog_platzi/src/actions/publicacionesActions.js
+++ blog_platzi/src/actions/publicacionesActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {
     CARGANDO,
     ERROR,
-    TRAER_POR_USUARIO
+    ACTUALIZAR
 } from '../types/publicacionesTypes';
 import * as usuariosTypes from '../types/usuariosTypes';
 
@@ -32,7 +32,7 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
         ];
 
         dispatch({
-            type: TRAER_POR_USUARIO,
+            type: ACTUALIZAR,
             payload: publicaciones_actualizadas
         });
 
@@ -57,6 +57,23 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
     }
 };
 
-export const abrirCerrar = (pub_key, com_key) => (dispatch) => {
-    console.log(pub_key, com_key);
+export const abrirCerrar = (pub_key, com_key) => (dispatch, getState) => {
+    const { publicaciones } = getState().publicacionesReducer;
+    const seleccionada = publicaciones[pub_key][com_key];
+
+    const actualizada = {
+        ...seleccionada,
+        abierto: !seleccionada.abierto
+    };
+
+    const publicaciones_actualizadas = [...publicaciones];
+    publicaciones_actualizadas[pub_key] = [
+        ...publicaciones[pub_key]
+    ];
+    publicaciones_actualizadas[pub_key][com_key] = actualizada;
+
+    dispatch({
+        type: ACTUALIZAR,
+        payload: publicaciones_actualizadas
+    });
 }
--- blog_platzi/src/components/Publicaciones/index.js
+++ blog_platzi/src/components/Publicaciones/index.js
@@ -95,6 +95,9 @@ class Publicaciones extends Component {
                 <h3>
                     {publicacion.body}
                 </h3>
+                {
+                    (publicacion.abierto) ? 'abierto' : 'cerrado'
+                }
             </div>
         ))
     )
