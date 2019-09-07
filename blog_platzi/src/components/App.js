e0e0d089b59dff692c3fb496af0e934e42ce61c0
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-09-06 22:46:26 -0500

    Parámetros por URL

--- blog_platzi/src/components/App.js
+++ blog_platzi/src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Menu from './Menu';
-import Usuarios from './Usuarios'
+import Usuarios from './Usuarios';
+import Publicaciones from './Publicaciones'
 
 const Tareas = () => <div>Tareas</div>
 const App = () => (
   <BrowserRouter>
     <Menu />
     <div className='margen'>
-      <Route exact path='/' component={Usuarios} />
-      <Route exact path='/tareas' component={Tareas} />
+      <Route exact path='/' component={ Usuarios } />
+      <Route exact path='/tareas' component={ Tareas } />
+      <Route exact path='/publicaciones/:key' component={ Publicaciones } />
     </div>
   </BrowserRouter>
 );
