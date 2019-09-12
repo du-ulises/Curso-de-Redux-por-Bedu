4ea21861c3c3384c4385bfa99bf699be320bbf4e
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-09-12 18:29:09 -0500

    Nuevo ciclo Redux

--- blog_platzi/src/components/App.js
+++ blog_platzi/src/components/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Menu from './Menu';
 import Usuarios from './Usuarios';
-import Publicaciones from './Publicaciones'
+import Publicaciones from './Publicaciones';
+import Tareas from './Tareas';
 
-const Tareas = () => <div>Tareas</div>
 const App = () => (
   <BrowserRouter>
     <Menu />
---
+++ blog_platzi/src/components/Tareas/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as tareasActions from '../../actions/tareasActions';
+
+class Tareas extends Component {
+    componentDidMount() {
+        this.props.traerTodas();
+    }
+    render() {
+        console.log(this.UNSAFE_componentWillReceiveProps);
+        
+        return (
+            <div>Tareas saludar</div>
+        );
+    }
+}
+
+const mapStateToProps = ({ tareasReducer }) => tareasReducer;
+
+export default connect(mapStateToProps, tareasActions)(Tareas);
